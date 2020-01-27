a8443f8531fb8783f070c1c97eee5bd7dec555cc
Kevin Lynch <kevin@technical.ly>
2020-01-26 20:33:14 -0500

    Added Wedding Planner App

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let interval = null;
 let words = ['Coding Enthusiast ','Product Analyst ','Competetive Gamer ','Mediocre Guitar Player '];
 function typeWriter() {
     let txt = words[i].substring(0,index + 1);
-    const element = document.getElementById("typewriter");
+    var element = document.getElementById("typewriter");
     element.innerHTML = txt;
     index++;
     if (txt === words[i]){
@@ -37,3 +37,57 @@ function typeWriter() {
 interval = setInterval(typeWriter,50);
 
 
+'use strict';
+
+var apiKey = "rbR0lm79ljgCAmxwn3jQTvjQRVvjGM7e1nYuH1q0"
+
+var baseURL = 'https://api.nps.gov/api/v1/parks?stateCode=';
+
+function formatQueryParams(params) {
+  var queryItems = params.map(element => `${encodeURIComponent(element)}=${encodeURIComponent(params[element])}`);
+  return queryItems.join('&');
+}
+
+function getNationalParks(stateCode, maxResults=10){
+  var queryString = formatQueryParams(stateCode);
+  var url = `${baseURL}${queryString}`;
+  console.log(url);
+
+  fetch (url)
+  .then (response => {
+    if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+  })
+  .then (responseJson => displayResults(responseJson, maxResults))
+  .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function displayResults(responseJson, maxResults) {
+  console.log(responseJson);
+  $('#results-list').empty();
+
+  for(let i=0;i<responseJson.data.length & i<maxResults ;i++) {
+    $('#results-list').append(
+      `<li><p>${responseJson.data[i].fullName}</p><p>${responseJson.data[i].description}</p><a href="${responseJson.data[i].url}" target="_blank">Website</a></li>`
+    )
+  }
+
+  $('#results').removeClass('hidden');
+}
+
+
+function watchForm() {
+$('form').submit(event => {
+  event.preventDefault();
+  var stateCode = $('#js-stateCode').val().split(',');
+  var maxResults = $('#js-max-results').val();
+  console.log(stateCode);
+  getNationalParks(stateCode, maxResults);
+})
+}
+
+$(watchForm());
